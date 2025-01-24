3f38e98ebc6a017e9b9afa1a36d144c90572760d
Rohan Ramidwar <rohanramidwar71@gmail.com>
2025-01-24 16:56:51 +0530

    routes setup and assets add

---
+++ app/sign-in.tsx
@@ -0,0 +1,20 @@
+import { View, Text, ScrollView, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "../constants/images";
+
+const SignIn = () => {
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
